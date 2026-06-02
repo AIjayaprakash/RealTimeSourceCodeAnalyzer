217931cb2f8cec9e162980e41205af7a455003f2
Langgraph_RAG <jayaeeegjprakash@gmail.com>
2026-06-02 13:27:34 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Realtime source code analyzer",
+    version="0.1.0",
+    author = "JP",
+    author_email="jayaeeegjprakash@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
